504f7d86966e5551180be8f1863e78820f4cb5b4
Fan-55 <openconvenience@gmail.com>
2025-02-04 23:11:03 +0800

    Add BreadthFirstPath class

---
+++ src/BreadthFirsPath.java
@@ -0,0 +1,63 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BreadthFirsPath {
+  private final boolean[] marked;
+  private final int[] edgeTo;
+  private final int source;
+
+  public BreadthFirsPath(UndirectedGraph G, int s) {
+    marked = new boolean[G.V()];
+    edgeTo = new int[G.V()];
+    this.source = s;
+    bfs(G, s);
+  }
+
+  private void bfs(UndirectedGraph G, int s) {
+    marked[s] = true;
+    Queue<Integer> queue = new Queue<>();
+    queue.enqueue(s);
+    while(!queue.isEmpty()) {
+      int v = queue.dequeue();
+      for (int w : G.adj(v)) {
+        if (!marked[w]) {
+          marked[w] = true;
+          edgeTo[w] = v;
+          queue.enqueue(w);
+        }
+      }
+    }
+  }
+
+  public boolean hasPathTo(int v) {
+    return marked[v];
+  }
+
+  public Iterable<Integer> pathTo(int v) {
+    if (!this.hasPathTo(v)) return null;
+    Stack<Integer> path = new Stack<>();
+    for (int x = v; x != this.source; x = edgeTo[x]) {
+      path.push(x);
+    }
+    path.push(this.source);
+    return path;
+  }
+
+  // Test client
+  public static void main(String[] args) {
+    UndirectedGraph G = new UndirectedGraph(new In(args[0]));
+    int s = Integer.parseInt(args[1]);
+    BreadthFirsPath search = new BreadthFirsPath(G, s);
+    for (int v = 0; v < G.V(); v++)
+    {
+      StdOut.print(s + " to " + v + ": ");
+      if (search.hasPathTo(v))
+        for (int x : search.pathTo(v))
+          if (x == s) StdOut.print(x);
+          else StdOut.print("-" + x);
+      StdOut.println();
+    }
+  }
+}
